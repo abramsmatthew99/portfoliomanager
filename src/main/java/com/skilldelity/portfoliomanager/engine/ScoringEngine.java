a62bce5decdb16db0a72f9d5477b19379b7b7dbd
TseringLhamo-prog <tsering.lhamo.work@gmail.com>
2026-01-22 13:22:25 -0500

    Added Recomendation Result

--- src/main/java/com/skilldelity/portfoliomanager/engine/ScoringEngine.java
+++ src/main/java/com/skilldelity/portfoliomanager/engine/ScoringEngine.java
@@ -1,5 +1,5 @@
 package com.skilldelity.portfoliomanager.engine;
 
 public class ScoringEngine {
-    
+
 }
--- src/main/java/com/skilldelity/portfoliomanager/model/RecommendationResult.java
+++ src/main/java/com/skilldelity/portfoliomanager/model/RecommendationResult.java
@@ -1,5 +1,36 @@
 package com.skilldelity.portfoliomanager.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * This represents the Buy / Sell / Hold recommendation for a single stock.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RecommendationResult {
-    
+
+    // The stock ticker symbol
+    private String ticker;
+
+    // The recommended action based on historical analysis.
+    private Action action;
+
+    // Confidence score (0–100) representing strength of the recommendation.
+
+    private int confidence;
+
+    // Explanation describing why this recommendation was made.
+    private String rationale;
+
+    // Allowed recommendation actions. Fixed list of allowed values.
+    public enum Action {
+        BUY,
+        SELL,
+        HOLD
+    }
 }
